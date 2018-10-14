9e7b273cd1cbae6dbbcb99f68dda1c12525252bd
JKoceski <41623717+JKoceski@users.noreply.github.com>
2018-10-14 23:08:29 +1100

    Add files via upload

--- WikiGame/index.php
+++ WikiGame/index.php
@@ -5,11 +5,11 @@
 <head lang="en">
     <meta charset="UTF-8">
     <title>Homepage</title>
-    <link rel="stylesheet" type="text/css" href="style.php"/>
+    <link href="format.css" rel="stylesheet">
 </head>
 <body>
 
-<h1>Wiki Game</h1>
+
 
 <ul>
     <li><a href="index.php">Home</a></li>
@@ -17,14 +17,16 @@
     <li><a href="signup.php">Sign Up</a></li>
 </ul>
 
-
-<?php if(!isset($_SESSION['username'])): ?>
-<p>In order to play you must <a href="login.php">sign in.</a> If you don't have an account you can register <a href="signup.php">here</a></p>
-<?php else: ?>
-<p>You are logged in as <?php if(isset($_SESSION['username'])) echo $_SESSION['username']; ?> <a href="logout.php">Logout</a></p>
-    <h3><a href="gamerules.html"> Play Game</a> </h3>
-<?php endif ?>
-
+<div id="signupContainer" class="container" style="height: 400px; top: 55%">
+    <h2>Wiki Game</h2>
+        <?php if(!isset($_SESSION['username'])): ?>
+            <h3>In order to play you must <a href="login.php">sign in.</a> If you don't have an account you can register <a href="signup.php">here</a></h3>
+        <?php else: ?>
+            <h3>You are logged in as <?php if(isset($_SESSION['username'])) echo $_SESSION['username']; ?> <a href="logout.php">Logout</a></h3>
+			<h3><a href="gamerules.html"> Play Game</a> </h3>
+        <?php endif ?>
+    
+</div>
 
 </body>
 </html>
